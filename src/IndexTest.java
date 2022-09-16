5df5af686f382cd7970f8cc4d3f203e1dc65046c
Gavin <gavoshmavo1@gmail.com>
2022-09-15 17:02:56 -0700

    Tested Remove
    remove tested unsuccessfully; files hashmap not initialized properly.

--- src/IndexTest.java
+++ src/IndexTest.java
@@ -42,19 +42,28 @@ class IndexTest {
 		ind.add("Test.txt");
 		Scanner sc = new Scanner("index");
 		String scanned = "";
-		for(scanned = ""; !scanned.equals("Test.txt : 8f0220cbd7ba37cd11f67dbd2bca2899e4368e80") && !sc.hasNextLine(); scanned = sc.nextLine()) {}
-		if(!sc.hasNextLine()) 
-			scanned = sc.nextLine();
+		for(scanned = ""; !scanned.equals("Test.txt : 8f0220cbd7ba37cd11f67dbd2bca2899e4368e80") && sc.hasNextLine(); scanned = sc.nextLine());
 		sc.close();
 		assertTrue(scanned.equals("Test.txt : 8f0220cbd7ba37cd11f67dbd2bca2899e4368e80"));
 		assertTrue(Files.exists(Paths.get("/objects/8f0220cbd7ba37cd11f67dbd2bca2899e4368e80")));
+		Scanner scWr = new Scanner("8f0220cbd7ba37cd11f67dbd2bca2899e4368e80");
+		for(scanned = scWr.nextLine(); scWr.hasNextLine(); scanned = scanned+"\n"+scWr.nextLine());
+		scWr.close();
+		System.out.println("Written in file: "+ scanned);
+		//
+		assertTrue(scanned.equals("This is a test."));
 	}
 	
 	@Test
 	void testRemoveFile() throws IOException {
 		Index ind = new Index();
 		ind.remove("Test.txt");
-		
+		assertFalse(Files.exists(Paths.get("8f0220cbd7ba37cd11f67dbd2bca2899e4368e80")));
+		String scanned = "";
+		Scanner sc = new Scanner("index");
+		for(scanned = ""; !scanned.equals("Test.txt : 8f0220cbd7ba37cd11f67dbd2bca2899e4368e80") && sc.hasNextLine(); scanned = sc.nextLine());
+		sc.close();
+		assertFalse(scanned.equals("Test.txt : 8f0220cbd7ba37cd11f67dbd2bca2899e4368e80"));
 	}
 
 }
